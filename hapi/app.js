3fc91ea7a281eaa68e2c2971ade3ba0db59e2302
mola1129 <ms.mola1129@gmail.com>
2021-10-23 01:15:16 +0900

    Add hapi files.

---
+++ hapi/app.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const Hapi = require("@hapi/hapi");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+  });
+
+  // routing
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Hello World!";
+    },
+  });
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+};
+
+// error handling
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
